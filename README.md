e7ab8949ad7bac313fc6fe7ac10d8e4e250675e1
Christopher Vigliotti <christopher@christophervigliotti.com>
2021-12-21 13:20:04 -0500

    work complete

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Instructions
 
-1. Add A button which says 'Display Details'
+1. Add a button which says 'Display Details'
 2. Add a paragraph with any content of your choice (e.g. 'Secret Password = tuna')
 3. Toggle the displaying of that paragraph with the button created in the first step
 4. Log all button clicks in an array and output that array below the secret paragraph (maybe log a timestamp or simply an incrementing number)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  // properties
+
+  doDisplayParagraph = false;
+  buttonPressLog = [];
+  htmlToAdd = '<div class="two">two</div>';
+  @ViewChild('div') div: ElementRef;
+
+  // methods
+
+  constructor(private renderer: Renderer2) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onDisplayToggle(event: any){
+    // console.log('onDisplayToggle()');
+    this.doDisplayParagraph = !this.doDisplayParagraph;
+    // console.log(' - this.doDisplayParagraph ' + this.doDisplayParagraph);
+    this.logHandler();
+    
+  }
+
+  logHandler(){
+    var count = this.buttonPressLog.length + 1;
+    var logMessage = '';
+    // logMessage = 'The button was pressed ' + count + ((count == 1)?' time':' times');
+    logMessage = 'The button was pressed for the ' + this.ordinal(count) + ' time';
+    logMessage = (count > 4)?('<div style="color:red;">' + logMessage + '!</div>'):logMessage + '.';
+    console.log(logMessage);
+    this.buttonPressLog.push(logMessage);
+    // this.htmlToAdd = logMessage;
+
+    const p: HTMLParagraphElement = this.renderer.createElement('p');
+    p.innerHTML = logMessage;
+    this.renderer.appendChild(this.div.nativeElement, p);
+
+  }
+
+  ordinal(n) {
+    var s = ["th", "st", "nd", "rd"];
+    var v = n%100;
+    return n + (s[(v-20)%10] || s[v] || s[0]);
+  }  
 }
